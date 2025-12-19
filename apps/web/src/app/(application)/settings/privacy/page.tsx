8a8dc8403351687169cf6c3aaaa6196264e6bcd8
Brandon Pérez <bperez@sistecredito.com>
2025-12-19 05:18:31 -0500

    privacy page added

---
+++ apps/web/src/app/(application)/settings/privacy/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from "@/components/header";
+
+export default function Privacy() {
+  return (
+    <section>
+      <Header>Privacidad y seguridad</Header>
+    </section>
+  );
+}
